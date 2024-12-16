ac9949c481aec70c17d89a5984e6e055a8ff44c1
abuhilmi156 <abuhilmiakun@gmail.com>
2024-12-16 10:57:23 +0700

    praktek part 2 - fungsi route, basic route, view route, route paramater, name route,  route controller

---
+++ buatblog/app/Http/Controllers/BlogController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class BlogController extends Controller
+{
+    function index()
+    {
+        return "Ini adalah halaman blog : Kisa-Kisah Para Sahabat";
+    }
+}
--- buatblog/routes/web.php
+++ buatblog/routes/web.php
@@ -1,7 +1,14 @@
 <?php
 
+
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\BlogController;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+// Route::get('/blog', function () {
+//     return view('blog');
+// });
+Route::get('/blog', [BlogController::class, 'index']);
